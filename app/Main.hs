cf8f5848c10bccafc22c266bbce55f6f6f0337b2
ronaldkuip <ronald.kuip@live.nl>
2021-11-15 13:46:12 +0100

    Zolder

--- app/Main.hs
+++ app/Main.hs
@@ -2,6 +2,20 @@ module Piet where
 
 x :: IO()
 x = putStrLn "33"
- 
+{- 
+
+>>> triple
+[3,4]
+>>> main
+>>> dtriple
+[2,3,4]
+
+-}
+triple :: [Int] 
+triple = [3,4]
+
+dtriple :: [Int] 
+dtriple = [2, 3,4]
+
 main :: IO()
 main = putStrLn "Dag wereld op zolder"
