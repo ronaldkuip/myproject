7a066aa6b45dc88ff1bd5fbb84e061760c44aa35
ronald kuip <ronald.kuip@live.nl>
2021-11-15 22:24:44 +0100

    Bridge

--- app/Main.hs
+++ app/Main.hs
@@ -1,26 +1,28 @@
 module Piet where
 
-x :: IO()
-x = putStrLn "33"
-{- 
-
->>> triple
-[3,4]
->>> main
->>> dtriple
-[2,3,4]
-
--}
-
--- Nu oveg ik beneden iets toe. Ik verwacht dat mijn client daarvan op de hoogte wordt gesteld.
-triple :: [Int] 
-triple = [3,4]
-
-dtriple :: [Int] 
-dtriple = [2, 3,4]
-
--- opnieuw ietes toeveogen - en weer een update van beneden
-
-
 main :: IO()
 main = putStrLn "Dag wereld op zolder"
+
+data Suit = Club | Diamond | Heart | Spade
+     deriving (Read, Show, Enum, Eq, Ord)
+
+data Rank = Two | Three | Four
+     | Five | Six | Seven | Eight | Nine | Ten 
+     | Jack | Queen | King | Ace
+    deriving (Read,  Show, Enum, Eq, Ord)
+
+data Card = Card { suit  :: Suit,
+                   rank  :: Rank }            
+    deriving (Read, Show, Eq)
+
+instance Ord Card where
+      compare c1 c2 = compare (rank c1, suit c1) (rank c2, suit c2)
+
+instance Enum Card where
+      toEnum   n  = let (suit,rank) = n `divMod` 13 in Card (toEnum suit) (toEnum rank)
+      fromEnum c  = fromEnum (suit c) * 13 + fromEnum (rank c)
+
+type Deck = [Card]
+
+deck::Deck
+deck = [Card suit rank | suit <- [Club .. Spade], rank <- [Two .. Ace] ]
